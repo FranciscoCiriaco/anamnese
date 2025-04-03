9368a948fdee4d7d8f2ae19fd48e161ab9f93fdc
FRANCISCO CIRIACO EVANGELISTA NETO <franscisco.ceneto@senacsp.edu.br>
2025-04-03 20:43:38 -0300

    primeiro commit

---
+++ src/main/java/br/com/anamnese/AnamneseSystemApplication.java
@@ -0,0 +1,13 @@
+package br.com.anamnese;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnamneseSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnamneseSystemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/anamnese/AnamneseSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.anamnese;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnamneseSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
